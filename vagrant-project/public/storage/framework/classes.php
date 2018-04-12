9fd1836962a7fddd23a9cf248a66971b6418fdb1
katelynsalem <katelynsalem@synemastudios.com>
2018-04-12 15:06:07 -0600

    Initial commit

---
+++ vagrant-project/public/storage/framework/classes.php
@@ -0,0 +1,12 @@
+<?php return array (
+  'october\\demo\\Plugin' => 'plugins/october/demo/Plugin.php',
+  'backend\\Controllers\\index' => 'modules/backend/controllers/index.php',
+  'backend\\Controllers\\auth' => 'modules/backend/controllers/auth.php',
+  'system\\Controllers\\updates' => 'modules/system/controllers/updates.php',
+  'cms\\Controllers\\themes' => 'modules/cms/controllers/themes.php',
+  'rainlab\\blog\\Plugin' => 'plugins/rainlab/blog/Plugin.php',
+  'rainlab\\pages\\Plugin' => 'plugins/rainlab/pages/Plugin.php',
+  'RainLab\\Blog\\Models\\Post' => 'plugins/rainlab/blog/models/Post.php',
+  'RainLab\\Blog\\Classes\\TagProcessor' => 'plugins/rainlab/blog/classes/TagProcessor.php',
+  'RainLab\\Blog\\Models\\Category' => 'plugins/rainlab/blog/models/Category.php',
+);
